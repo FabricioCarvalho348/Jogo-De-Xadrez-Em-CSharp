c4b0f6158ff1ae0235518fffcaf9bcc2a3d80bd3
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-09-16 12:54:05 -0300

    Feat: Adding Position class.

---
+++ xadrez_console/tabuleiro/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace xadrez_console.tabuleiro;
+
+public class Position
+{
+    public int row { get; set; }
+    public int column { get; set; }
+
+    public Position(int row, int column)
+    {
+        this.row = row;
+        this.column = column;
+    }
+
+    public override string ToString()
+    {
+        return row
+               + ", "
+               + column;
+    }
+}
