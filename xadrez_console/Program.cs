d59c0e5a17396c6c8a025d0abfebbc481fa7cdd2
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-09-16 14:23:49 -0300

    feat: Creating the chess position class, so that the board positions follow the position of the chessboard and not the programming vectors.

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,21 +8,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        try
-        {
-            Board board = new Board(8, 8);
-        
-            board.PlacePiece(new Rook(board, Color.Black) , new Position(0, 0));
-            board.PlacePiece(new Rook(board, Color.Black) , new Position(1, 3));
-            board.PlacePiece(new King(board, Color.Black) , new Position(2, 4));
-        
-            Screen.PrintBoard(board);
-        }
-        catch (BoardException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        ChessPosition pos = new ChessPosition('a', 1);
 
+        Console.WriteLine(pos);
+
+        Console.WriteLine(pos.ToPosition());
+        
         Console.ReadLine();
     }
 }
