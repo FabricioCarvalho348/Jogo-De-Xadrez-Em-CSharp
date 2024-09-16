378c88cacb649992216531d3cc270c9f005d2d96
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-09-16 14:33:33 -0300

    Feat: Improving the implementation of the PrintBoard method

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,12 +8,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        ChessPosition pos = new ChessPosition('a', 1);
-
-        Console.WriteLine(pos);
-
-        Console.WriteLine(pos.ToPosition());
+        try
+        {
+            Board board = new Board(8, 8);
         
+            board.PlacePiece(new Rook(board, Color.Black) , new Position(0, 0));
+            board.PlacePiece(new Rook(board, Color.Black) , new Position(1, 3));
+            board.PlacePiece(new King(board, Color.Black) , new Position(2, 4));
+            
+            board.PlacePiece(new King(board, Color.White) , new Position(3, 5));
+        
+            Screen.PrintBoard(board);
+        }
+        catch (BoardException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
         Console.ReadLine();
     }
 }
