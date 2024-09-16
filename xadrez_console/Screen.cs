c48bf18f2c42f9ab8e7c3816e0cf26f608367a2d
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-09-16 14:33:56 -0300

    Feat: Improving the implementation of the PrintBoard method

--- xadrez_console/Screen.cs
+++ xadrez_console/Screen.cs
@@ -8,6 +8,7 @@ public class Screen
     {
         for (int i = 0; i < board.Lines; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < board.Columns; j++)
             {
                 if (board.Piece(i, j) == null)
@@ -16,10 +17,27 @@ public class Screen
                 }
                 else
                 {
-                    Console.Write(board.Piece(i, j) + " ");
+                    PrintPiece(board.Piece(i, j));
+                    Console.Write(" ");
                 }
             }
             Console.WriteLine();
         }
-    } 
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if (piece.Color == Color.White)
+        {
+            Console.Write(piece);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
+        }
+    }
 }
